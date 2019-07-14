1a8ef579e9e018dafb43aaa9ee063e4cccf6e503
jian <janine.jian.chen@gmail.com>
2019-07-14 16:04:00 +0800

    add array module code

---
+++ [1051]height-checker/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] heights = {1,1,4,2,1,3};
+        int i = solution.heightChecker(heights);
+        System.out.println(i);
+    }
+}
