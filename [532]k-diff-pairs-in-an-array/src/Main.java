12c42ae92399d67c76168e18188530788bbe8d44
jian <janine.jian.chen@gmail.com>
2019-07-31 19:38:06 +0800

    [532]k-diff-pairs-in-an-array

---
+++ [532]k-diff-pairs-in-an-array/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] nums = {1,1,1,1,1};
+        int k = 0;
+        System.out.print(solution.findPairs(nums,k));
+    }
+}
