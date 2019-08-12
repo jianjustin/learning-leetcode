a8bd6809a3d8d066adf191abe75c02483b7a84c5
jian <janine.jian.chen@gmail.com>
2019-08-12 16:52:06 +0800

    [28]implement-strstr

---
+++ [28]implement-strstr/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        String haystack = "hello", needle = "ll";
+        System.out.print(solution.strStr(haystack,needle));
+    }
+
+}
