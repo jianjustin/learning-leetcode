f4eca30b87509ec861dc3e2bdb1cf59a2aa9c710
jian <janine.jian.chen@gmail.com>
2019-08-17 14:06:16 +0800

    [345]reverse-vowels-of-a-string

--- [345]reverse-vowels-of-a-string/src/Main.java
+++ [345]reverse-vowels-of-a-string/src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        String s = "hello";
+        System.out.print(solution.reverseVowels(s));
+    }
 }
--- [345]reverse-vowels-of-a-string/src/Solution.java
+++ [345]reverse-vowels-of-a-string/src/Solution.java
@@ -1,4 +1,28 @@
 public class Solution {
 
+    public String reverseVowels(String s) {
+        char[] chars = s.toCharArray();
+        int m = 0 , n = s.length()-1;
+
+        while (m < n){
+            while (m < s.length() && !isvowel(chars[m]))m++;
+            if (m >= n)break;
+            while (n < s.length() && !isvowel(chars[n]))n--;
+            if (m >= n)break;
+            if (isvowel(chars[m]) && isvowel(chars[n])){
+                char temp = chars[n];
+                chars[n--] = chars[m];
+                chars[m++] = temp;
+            }
+        }
+
+        return new String(chars);
+    }
+
+    public boolean isvowel(char c){
+        String s = "aeiouAEIOU";
+        return s.indexOf(Character.toString(c)) > -1;
+    }
+
 
 }
