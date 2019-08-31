94e094daf77c1bb3257eba0b7add3ecf10714348
jian <janine.jian.chen@gmail.com>
2019-08-31 22:08:55 +0800

    [746]min-cost-climbing-stairs

--- [746]min-cost-climbing-stairs/src/Main.java
+++ [746]min-cost-climbing-stairs/src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] a = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+        System.out.println(solution.minCostClimbingStairs(a));
+    }
 }
--- [746]min-cost-climbing-stairs/src/Solution.java
+++ [746]min-cost-climbing-stairs/src/Solution.java
@@ -1,10 +1,19 @@
 public class Solution {
 
     public int minCostClimbingStairs(int[] cost) {
-        int min = 0;
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.length; i++)
+            dp[i] = min(dp[i-2],dp[i-1]) + cost[i];
+        return min(dp[cost.length-1],dp[cost.length-2]);
 
-        return min;
+    }
 
+    public int min(int a , int b){
+        if (a > b)
+            return b;
+        return a;
     }
 
 }
