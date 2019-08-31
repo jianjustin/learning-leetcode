43f64a6e020f36abdb533ef1866fb849c7bbaabc
jian <janine.jian.chen@gmail.com>
2019-08-31 09:28:18 +0800

    [392]is-subsequence

---
+++ [392]is-subsequence/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "axc" , t = "ahbgdc";
+        System.out.println(solution.isSubsequence(s,t));
+    }
+}
---
+++ [392]is-subsequence/src/Solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+
+    public boolean isSubsequence(String s, String t) {
+        int sLen = s.length(), tLen = t.length();
+        if (sLen > tLen) return false;
+        if (sLen == 0) return true;
+        boolean[][] dp = new boolean[sLen + 1][tLen + 1];
+        //初始化
+        for (int j = 0; j < tLen; j++)
+            dp[0][j] = true;
+
+        //dp
+        for (int i = 1; i <= sLen; i++) {
+            for (int j = 1; j <= tLen; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[sLen][tLen];
+
+    }
+
+}
