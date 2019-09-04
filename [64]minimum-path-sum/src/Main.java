7b622cc05299d0f8c8def78d231c66d697e8fafc
jian <janine.jian.chen@gmail.com>
2019-09-04 23:04:15 +0800

    [64]minimum-path-sum

---
+++ [64]minimum-path-sum/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] a = {{1,3,1},{1,5,1},{4,2,1}};
+        solution.minPathSum(a);
+    }
+}
