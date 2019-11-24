53d5225b2af66690f38796a57fdd2a043ae30061
jian <janine.jian.chen@gmail.com>
2019-11-24 19:46:38 +0800

    [3]longest-common-subsequence

--- [15]3sum/src/Solution.java
+++ [15]3sum/src/Solution.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Solution {
-    public List<List<Integer>> threeSum1(int[] nums) {
+    public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList();
         if (null == nums || nums.length < 3)return result;//前置条件
         Arrays.sort(nums);
@@ -30,35 +29,10 @@ public class Solution {
         return result;
     }
 
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList();
-        if(null == nums || nums.length < 3) return ans;
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length ; i++) {
-            if(nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
-            if(i > 0 && nums[i] == nums[i-1]) continue; // 去重
-            int L = i+1;
-            int R = nums.length-1;
-            while(L < R){
-                int sum = nums[i] + nums[L] + nums[R];
-                if(sum == 0){
-                    ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
-                    while (L<R && nums[L] == nums[L+1]) L++; // 去重
-                    while (L<R && nums[R] == nums[R-1]) R--; // 去重
-                    L++;
-                    R--;
-                }
-                else if (sum < 0) L++;
-                else if (sum > 0) R--;
-            }
-        }
-        return ans;
-    }
-
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums = {0,0,0};
-        List<List<Integer>> list = solution.threeSum1(nums);
+        List<List<Integer>> list = solution.threeSum(nums);
         System.out.println(Arrays.toString(list.toArray()));
     }
 }
--- [1]two-sum/src/Solution.java
+++ [1]two-sum/src/Solution.java
@@ -2,34 +2,23 @@ public class Solution {
 
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-
         for (int i = 0; i < nums.length; i++) {
-            //if(nums[i] > target)continue;
             for (int j = i+1; j < nums.length; j++) {
-                //if(nums[j] > target)continue;
                 if(target == (nums[i]+nums[j])){
                     result[0] = i;
                     result[1] = j;
                 }
-
             }
-
         }
-
         return result;
     }
 
-    public static void insertSort(int[] a) {
-        int i, j, insertNote;
-        for (i = 1; i < a.length; i++) {
-            insertNote = a[i];
-            j = i - 1;
-            while (j >= 0 && insertNote < a[j]) {
-                a[j + 1] = a[j];
-                j--;
-            }
-            a[j + 1] = insertNote;
-        }
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] a = {-3,4,3,90};
+        int target = 0;
+        int[] result = solution.twoSum(a,target);
+        System.out.println(result[0]+"    "+result[1]);
     }
 
 
