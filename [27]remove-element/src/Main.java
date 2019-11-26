49ed27d77f292d11c13d35e93e4eb2c53149222c
jian <janine.jian.chen@gmail.com>
2019-11-26 11:12:51 +0800

    feat : clean code [27]remove-element

--- [27]remove-element/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args){
-        Solution solution = new Solution();
-        int[] nums = {4,5};
-        int val = 4;
-        System.out.print(solution.removeElement(nums,val));
-    }
-}
--- [27]remove-element/src/Solution.java
+++ [27]remove-element/src/Solution.java
@@ -5,13 +5,19 @@ public class Solution {
         if (len == 0)return len;
         for (int i = 0; i < len; i++) {
             if(nums[i] == val){
-                while (len > 0 && nums[len-1] == val)
-                    len--;
+                while (len > 0 && nums[len-1] == val)len--;
                 if(i >= len )break;
                 nums[i] = nums[--len];
             }
         }
         return len;
+    }
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] nums = {4,5};
+        int val = 4;
+        System.out.print(solution.removeElement(nums,val));
     }
 
 }
