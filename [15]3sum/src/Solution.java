998a09cd0844e488fd73c033789b65c1bf73a561
jian <janine.jian.chen@gmail.com>
2019-12-28 21:41:23 +0800

    clean code

--- [15]3sum/src/Solution.java
+++ [15]3sum/src/Solution.java
@@ -25,14 +25,32 @@ public class Solution {
                 else if (sum > 0) k--;
             }
         }
+        return result;
+    }
 
+    public List<List<Integer>> threeSum1(int[] nums) {
+        List<List<Integer>> result = new ArrayList();
+        if (null == nums || nums.length < 3)return result;//前置条件
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i > 0 && nums[i] == nums[i-1])continue;
+            for (int j = i+1; j < nums.length-1; j++) {
+                if (j > i+1 && nums[j] == nums[j-1])continue;
+                for (int k = j+1; k < nums.length; k++) {
+                    if (k > j+1 && nums[k] == nums[k-1])continue;
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (0 == sum)result.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                }
+            }
+        }
         return result;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums = {0,0,0};
-        List<List<Integer>> list = solution.threeSum(nums);
+        List<List<Integer>> list = solution.threeSum1(nums);
         System.out.println(Arrays.toString(list.toArray()));
     }
 }
--- [27]remove-element/src/Solution.java
+++ [27]remove-element/src/Solution.java
@@ -1,23 +1,15 @@
 public class Solution {
 
     public int removeElement(int[] nums, int val) {
+        if (null == nums || nums.length == 0)return 0;
         int len = nums.length;
-        if (len == 0)return len;
         for (int i = 0; i < len; i++) {
-            if(nums[i] == val){
-                while (len > 0 && nums[len-1] == val)len--;
-                if(i >= len )break;
+            if (nums[i] == val) {
+                while (len > 0 && nums[len - 1] == val)len--;
+                if (i >= len) break;
                 nums[i] = nums[--len];
             }
         }
         return len;
     }
-
-    public static void main(String[] args){
-        Solution solution = new Solution();
-        int[] nums = {4,5};
-        int val = 4;
-        System.out.print(solution.removeElement(nums,val));
-    }
-
 }
