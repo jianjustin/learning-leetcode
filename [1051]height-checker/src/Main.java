f92413eaaa143ecaf6c51c2ef26fbfba815a2b6b
jian <janine.jian.chen@gmail.com>
2019-12-28 21:46:30 +0800

    del all useless code

--- [1051]height-checker/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args){
-        Solution solution = new Solution();
-        int[] heights = {1,1,4,2,1,3};
-        int i = solution.heightChecker(heights);
-        System.out.println(i);
-    }
-}
