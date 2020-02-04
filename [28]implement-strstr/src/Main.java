528106eab22a18a1a381008f0bd461522463474d
jian <janine.jian.chen@gmail.com>
2020-02-04 17:42:33 +0800

    add solution [28]implement-strstr

--- [28]implement-strstr/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args){
-        Solution solution = new Solution();
-        String haystack = "hello", needle = "ll";
-        System.out.print(solution.strStr(haystack,needle));
-    }
-
-}
--- [28]implement-strstr/src/Solution.java
+++ [28]implement-strstr/src/Solution.java
@@ -1,7 +1,53 @@
 public class Solution {
 
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        if (null == needle || needle.length() == 0)return 0;
+        int  lps[] = new int[needle.length()] , j = 0 , i = 0;
+
+        char[] haystacks = haystack.toCharArray() , needles = needle.toCharArray();
+
+        computeLPSArray(needle, lps);//生成字符匹配表
+
+        while (i < haystack.length()) {
+            if (needles[j] == haystacks[i]) {
+                j++;
+                i++;
+            }
+            if (j == needle.length()) {
+                return i-j;
+            }else if (i < haystack.length() && needles[j] != haystacks[i]) {
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+        return -1;
+    }
+
+    void computeLPSArray(String pat, int lps[]){
+        int i = 1 , j = 0 ;
+
+        while (i < pat.length()) {
+            if (pat.charAt(i) == pat.charAt(j)) {
+                lps[i++] = ++j;//因为j从0开始计数，所以需要先计算j的值
+            } else {
+                if (j != 0)
+                    j = lps[j - 1];
+                 else
+                    lps[i++] = j;
+
+            }
+        }
+    }
+
+
+    public static void main(String args[])
+    {
+        String txt = "ABABDABACDABABCABAB";
+        String pat = "ABABCABAB";
+        Solution solution = new Solution();
+        System.out.println(solution.strStr(txt,pat));
     }
 
 }
